27ecb351a2a397029f5b555d746235bf970ba03d
Saeyoon Oh <greentea@Saeyoons-MacBook-Air.local>
2023-03-20 15:26:00 +0900

    push analyze module

--- utils.py
+++ utils.py
@@ -1,17 +1,19 @@
 import os
 import ast
 import glob
+from collections import defaultdict
 
 
 def get_class_defs(model_dir):
     # root_dir needs a trailing slash (i.e. /root/dir/)
-    class_defs = []
+    class_defs = defaultdict(lambda: "")
     for filename in glob.iglob(model_dir + "**/*.py", recursive=True):
         with open(filename, "r") as f:
             file_ast = ast.parse(f.read())
-        import ipdb
-
-        ipdb.set_trace()
+        for stmt in file_ast.body:
+            if type(stmt) == ast.ClassDef:
+                class_defs[filename + ":" + stmt.name] = stmt
+    return class_defs
 
 
 def analyze_model(model, model_dir, torch_dir=None):
@@ -28,4 +30,7 @@ def analyze_model(model, model_dir, torch_dir=None):
             target_modules.add(target_module)
     print(target_modules)
     print("\n\n")
-    get_class_defs(model_dir)
+    class_defs = get_class_defs(model_dir)
+    import ipdb
+
+    ipdb.set_trace()
