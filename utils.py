077762ee8c38f2f52a3da7375bdd670ff648d46d
Saeyoon Oh <greentea@Saeyoons-MacBook-Air.local>
2023-03-20 17:40:42 +0900

    get functions defs

--- utils.py
+++ utils.py
@@ -30,12 +30,12 @@ def match_external_funcs(class_defs):
             try:
                 # if the function is __init__,
                 if body.name == "__init__":
+                    import ipdb
+
+                    ipdb.set_trace()
                     init_body = body
                     # for each stmt in init_body,
                     for stmt in init_body:
-                        import ipdb
-
-                        ipdb.set_trace()
                         # if the statement is assign, and its value is function call, and is external
                         if (
                             type(stmt) == ast.Assign
