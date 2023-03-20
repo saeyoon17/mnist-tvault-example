fa9d4c3176dc7b257481222e7b5a137a5aaf2901
Saeyoon Oh <greentea@Saeyoons-MacBook-Air.local>
2023-03-20 14:47:08 +0900

    push analyze module

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ import torch.distributed as dist
 import torch.optim as optim
 import torchvision
 import numpy as np
+from utils import analyze_model
 import torch.nn.functional as F
 
 from torch.nn.parallel import DistributedDataParallel as DDP
@@ -58,7 +59,11 @@ def test(model, test_loader, local_rank, criterion):
             pred = output.data.max(1, keepdim=True)[1]
             correct += pred.eq(target.data.view_as(pred)).sum()
     test_loss /= len(test_loader.dataset)
-    print("\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)))
+    print(
+        "\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
+        )
+    )
 
 
 def get_args_parser():
@@ -93,7 +98,15 @@ if __name__ == "__main__":
 
     # Model
     train_dataset = torchvision.datasets.MNIST(
-        "/MNIST/", train=True, download=True, transform=torchvision.transforms.Compose([torchvision.transforms.ToTensor(), torchvision.transforms.Normalize((0.1307,), (0.3081,))])
+        "/MNIST/",
+        train=True,
+        download=True,
+        transform=torchvision.transforms.Compose(
+            [
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        ),
     )
     train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
     train_loader = torch.utils.data.DataLoader(
@@ -103,13 +116,24 @@ if __name__ == "__main__":
     )
 
     test_dataset = torchvision.datasets.MNIST(
-        "/MNIST/", train=False, download=True, transform=torchvision.transforms.Compose([torchvision.transforms.ToTensor(), torchvision.transforms.Normalize((0.1307,), (0.3081,))])
+        "/MNIST/",
+        train=False,
+        download=True,
+        transform=torchvision.transforms.Compose(
+            [
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        ),
     )
     test_loader = torch.utils.data.DataLoader(
         test_dataset,
         batch_size=batch_size,
     )
     model = resnet18(10)
+    import ipdb
+
+    ipdb.set_trace()
     model = model.to(args.local_rank)
     model = DDP(model, device_ids=[args.local_rank])
     criterion = torch.nn.NLLLoss()
---
+++ utils.py
@@ -0,0 +1,17 @@
+import os
+import ast
+
+
+def analyze_model(model):
+    model = model.__str__
+    target_modules = []
+    for line in model:
+        if "(" in line:
+            if "\t" not in line:
+                # model classname
+                target_module = line.split("(")[0]
+            else:
+                # submodules
+                target_module = line.split("(")[1].split(" ")[-1]
+            target_modules.append(target_module)
+    print(target_modules)
