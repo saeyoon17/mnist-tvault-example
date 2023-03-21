bf859ad514480dd5da8510efa55148e407e232de
Saeyoon Oh <greentea@Saeyoons-MacBook-Air.local>
2023-03-21 10:40:45 +0900

    ver 3 - change external function conv3x3

--- module/model.py
+++ module/model.py
@@ -26,7 +26,7 @@ class Net(nn.Module):
 
 def conv3x3(in_planes, out_planes, stride=1):
     """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+    return nn.Conv2d(in_planes, out_planes, kernel_size=10, stride=stride, padding=1, bias=False)
 
 
 class BasicBlock(nn.Module):
